1fe47b66be11fd8e5f534031bc0e9fc86822eb27
Yadunandan Ahuja <yaduahuja24@gmail.com>
2022-08-01 16:40:33 +0530

    Fixed Dependency Issues

--- src/appInsights.ts
+++ src/appInsights.ts
@@ -1,13 +1,13 @@
-import * as applicationinsights from 'applicationinsights';
+// import * as applicationinsights from 'applicationinsights';
 
-export class AppInsights {
-	private _client: applicationinsights.TelemetryClient;
-	constructor() {
-		applicationinsights.setup('InstrumentationKey=b52b34ce-69dc-46a1-9f40-a6c4d827bd60;IngestionEndpoint=https://centralindia-0.in.applicationinsights.azure.com/');
-		this._client = applicationinsights.defaultClient;
-	}
+// export class AppInsights {
+// 	private _client: applicationinsights.TelemetryClient;
+// 	constructor() {
+// 		applicationinsights.setup('InstrumentationKey=b52b34ce-69dc-46a1-9f40-a6c4d827bd60;IngestionEndpoint=https://centralindia-0.in.applicationinsights.azure.com/');
+// 		this._client = applicationinsights.defaultClient;
+// 	}
 
-	public async sendEvent(eventName: string, properties: { [key: string]: any }): Promise<void> {
-		this._client.trackEvent({ name: eventName, properties: properties });
-	}
-}
+// 	public async sendEvent(eventName: string, properties: { [key: string]: any }): Promise<void> {
+// 		this._client.trackEvent({ name: eventName, properties: properties });
+// 	}
+// }
